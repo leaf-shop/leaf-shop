d025a9ebdd346403d0e040066a71425b51e5d635
rasool khorshidi <r98kh@yahoo.com>
2023-11-06 14:04:17 +0330

    product model

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ['is_active']
+    list_display = ['title', 'price', 'is_active', 'count']
+    list_editable = ['price', 'is_active', 'count']
+
+
+admin.site.register(models.Product, ProductAdmin)
--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from slugify import slugify
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    cover = models.ImageField(upload_to='images/product')
+    description = models.TextField(db_index=True)
+    price = models.FloatField()
+    count = models.IntegerField()
+    slug = models.SlugField(null=False, db_index=True, blank=True, max_length=200, unique=True)
+    is_active = models.BooleanField(default=False)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
