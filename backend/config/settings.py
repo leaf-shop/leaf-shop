7c11be42e180924bbae81bf1be55f54003f56ef7
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-06 19:49:23 +0330

    add swagger

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # Apps
     'drf_yasg',
     'product',
+    'discount',
 ]
 
 MIDDLEWARE = [
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', swagger.schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
