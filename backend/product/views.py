227790f2c62dc892ef9a6261fa0ed10cea93c73e
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-06 20:30:56 +0330

    add product view

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from . import models, serializers
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
