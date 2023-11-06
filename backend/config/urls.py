a27f6f982a24bd576e1f30879f87a7f4850365ff
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-06 20:38:52 +0330

    modify urls of config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import swagger
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("/", include('discount.urls')),
     path("/", include('product.urls')),
+    path("/", include('attribute.urls')),
     path('swagger/', swagger.schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
