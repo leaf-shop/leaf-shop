a7f7b855f13ba3b5abbd3280517b0de8ed7a3908
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-06 20:31:53 +0330

    modify urls of discount

--- backend/discount/urls.py
+++ backend/discount/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 route = DefaultRouter()
-route.register("discount", views.DiscountViewset)
+route.register("discount", views.DiscountViewSet)
 
 urlpatterns = route.urls
