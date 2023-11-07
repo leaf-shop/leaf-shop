bafd0c45d04b91a3b4198d720acc84116edd0323
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-07 18:53:32 +0330

    add fields to discount model

--- backend/discount/models.py
+++ backend/discount/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 class Discount(models.Model):
+    DISCOUNT_TYPES = (
+        ("n", "Number"),
+        ("p", "Percent")
+    )
     title = models.CharField(max_length=100)
     value = models.PositiveIntegerField()
-    
+    type = models.CharField(max_length=1, choices=DISCOUNT_TYPES)
+    start_date = models.DateTimeField()
+    expire_date = models.DateTimeField()
