66ac7f4952e074ebd0d127cddf7d8d2f3d2c150f
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-07 18:54:03 +0330

    modify product

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 from slugify import slugify
+from discount.models import Discount
+from attribute.models import Attribute
 
 # Create your models here.
 
+
 class Product(models.Model):
     title = models.CharField(max_length=50)
-    cover = models.ImageField(upload_to='images/product')
+    cover = models.ImageField(upload_to='images/product', null=True, blank=True)
     description = models.TextField(db_index=True)
     price = models.FloatField()
+    discount = models.ForeignKey(
+        Discount, on_delete=models.CASCADE, null=True, blank=True, related_name="products")
+    attribute = models.ManyToManyField(
+        Attribute, null=True, blank=True, related_name="products")
     count = models.IntegerField()
-    slug = models.SlugField(null=False, db_index=True, blank=True, max_length=200, unique=True)
+    slug = models.SlugField(null=False, db_index=True,
+                            blank=True, max_length=200, unique=True)
     is_active = models.BooleanField(default=False)
-
 
     def save(self, *args, **kwargs):
         if not self.slug:
