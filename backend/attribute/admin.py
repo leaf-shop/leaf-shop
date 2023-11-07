564c502c2aabcf43ffc249b3c6b8e5d56344d654
rasool khorshidi <r98kh@yahoo.com>
2023-11-07 16:34:57 +0330

    add attribute and discount to product model

--- backend/attribute/admin.py
+++ backend/attribute/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class AttributeAdmin(admin.ModelAdmin):
+    list_display = ['key', 'value']
+
+
+admin.site.register(models.Attribute, AttributeAdmin)
