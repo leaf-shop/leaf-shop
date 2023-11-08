a55ce34666b710cbe32094042426bba74516b40c
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-08 15:37:43 +0330

    move comment to shared views

--- backend/share_module/views.py
+++ backend/share_module/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from . import models, serializers
-# Create your views here.
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
