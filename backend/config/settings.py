602b02bed94634d14f36fe2eb5f5b1fb533712c8
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-11 18:58:02 +0330

    add user model and installed user app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'product',
     'discount',
     'share_module',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
