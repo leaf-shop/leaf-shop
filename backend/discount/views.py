033e4a90e87345889001d71997dee116a125f31a
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-11 18:58:30 +0330

    add permission for discount

--- backend/discount/views.py
+++ backend/discount/views.py
@@ -1,7 +1,12 @@
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework import viewsets
 from . import models, serializers
 
 
 class DiscountViewSet(viewsets.ModelViewSet):
+    
     queryset = models.Discount.objects.all()
     serializer_class = serializers.DiscountSerializer
+
+    def get_permissions(self):
+        return [AllowAny()] if self.request.method == "GET" else [IsAdminUser()]
