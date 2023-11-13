0a46280e1304fe7a00f107410420020f592448e7
rasool khorshidi <r98kh@yahoo.com>
2023-11-13 07:51:25 +0330

    add view and serializer for blog

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . import models, serializers
 # Create your views here.
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = models.Blog.objects.all()
+    serializer_class = serializers.BlogSerializer
