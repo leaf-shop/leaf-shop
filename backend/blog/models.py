eacadb9f0e1cc517874118545fa95b3f9522d1c6
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-13 13:55:34 +0330

    improvements for blog model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -3,21 +3,24 @@ from django.contrib.auth import get_user_model
 from slugify import slugify
 from shared.models import Category
 
-# Create your models here.
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
     cover = models.ImageField(upload_to='images/blog')
     text = models.TextField()
-    category = models.ManyToManyField(Category)
+    category = models.ManyToManyField(Category, blank=True)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_datetime = models.DateTimeField(auto_now_add=True)
-    updated_datetime = models.DateTimeField(null=True,blank=True)
+    updated_datetime = models.DateTimeField(null=True, blank=True)
     is_active = models.BooleanField(default=False)
     slug = models.SlugField(null=False, db_index=True,
                             blank=True, max_length=200, unique=True)
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
