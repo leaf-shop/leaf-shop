f0b7d034b9092a54d3eefed3f23b4a0896859b7c
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-13 13:55:20 +0330

    add media root address

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
