93e429d415693b746ca9c775b097d9de6409c323
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-11-15 20:21:10 +0330

    add paginations

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,3 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 12,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+}
