7d827b689156c5e31fb7e5b9d457c955d6c403d9
behradbhrmi <behrad.bhrmi@gmail.com>
2023-12-16 18:50:00 +0330

    some improvemnets and add parameter to url instead of taking it from request

--- backend/support/views.py
+++ backend/support/views.py
@@ -16,20 +16,12 @@ class TicketViewSet(viewsets.ModelViewSet):
             return serializers.TicketOutputSerializer
         return serializers.TicketInputSerializer
 
-    @action(detail=False, methods=['get'])
-    def tickets_by_assignee(self, request):
-        assignee_id = self.request.GET.get("id")
-        if not assignee_id:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    def tickets_by_assignee(self, request, assignee_id):
         data = self.queryset.filter(assignee_id=assignee_id)
         serializer = self.get_serializer(data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(detail=False, methods=['get'])
-    def tickets_by_creator(self, request):
-        creator_id = self.request.GET.get("id")
-        if not creator_id:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    def tickets_by_creator(self, request, creator_id):
         data = self.queryset.filter(creator_id=creator_id)
         serializer = self.get_serializer(data, many=True)
         return Response(serializer.data)
@@ -44,11 +36,7 @@ class CommentViewSet(viewsets.ModelViewSet):
             return serializers.TicketCommentOutputSerializer
         return serializers.TicketCommentInputSerializer
 
-    @action(detail=False, methods=['get'])
-    def comments_by_creator(self, request):
-        creator_id = self.request.GET.get("id")
-        if not creator_id:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    def comments_by_creator(self, request, creator_id):
         data = self.queryset.filter(author_id=creator_id)
         serializer = self.get_serializer(data, many=True)
         return Response(serializer.data)
