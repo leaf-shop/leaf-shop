d4e3da9dc2d35c6cb74475a22fffd33f59e1f06f
behradbhrmi <behrad.bhrmi@gmail.com>
2023-12-17 11:47:06 +0330

    added favorites product for user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,8 @@
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 
 class CustomUser(AbstractUser):
     phone_number = PhoneNumberField(null=True, blank=True, unique=True)
+    favorites = models.ManyToManyField("product.Product", blank=True)
