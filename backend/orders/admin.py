654d048734fe721d4ff926d888d7bb404c668e41
behradbhrmi <behrad.bhrmi@gmail.com>
2023-12-18 08:06:55 +0330

    added admin classes for orders

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "status", "user", "datetime_created",]
+
+@admin.register(models.OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["order", "product", "quantity"]
