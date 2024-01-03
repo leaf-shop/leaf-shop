2acc59aa137f677992d381c10bfd37637a58d186
behradbhrmi <behradbhrmi@.com>
2024-01-03 12:03:10 +0330

    fix request counter middleware

--- backend/statistic/middleware.py
+++ backend/statistic/middleware.py
@@ -1,3 +1,5 @@
+from django.contrib.sessions.models import Session
+from django.utils import timezone
 from datetime import date
 from django.db import IntegrityError
 from .models import DailyRequestCount
@@ -13,10 +15,14 @@ class RequestCounterMiddleware:
 
     def __call__(self, request):
         try:
-            today = date.today()
-            daily_request_count, created = DailyRequestCount.objects.get_or_create(date=today)
-            daily_request_count.count += 1
-            daily_request_count.save()
+            if not hasattr(request, "request_counted"):
+                today = date.today()
+                daily_request_count, created = DailyRequestCount.objects.get_or_create(date=today)
+                daily_request_count.count += 1
+                daily_request_count.save()
+                request.request_counted = True
+            else:
+                delattr(request, "request_counted")     
 
         except IntegrityError:
             logger.warning("IntegrityError in RequestCounterMiddleware")
